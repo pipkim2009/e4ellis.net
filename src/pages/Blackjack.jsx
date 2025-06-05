8f0303cde0364c198076b777f31d7a5b9d2abdcc
Ellis <e4ellis09@gmail.com>
2025-06-05 21:57:35 +0100

    blackjack react edition development

--- src/pages/Blackjack.jsx
+++ src/pages/Blackjack.jsx
@@ -7,7 +7,29 @@ export function Blackjack() {
 
     return (
         <>
-            {active ? null : <ProjectMenu setActiveL={setActive} />}
+            {active ?
+
+            <section id="bj-section">
+                <h1 id="project-title">BlackJack</h1>
+                <div id="bj-button-row">
+                    <button id="bj-deal-button">Hit</button>
+                    <button id="bj-hit-button">Restart</button>
+                    <button id="bj-stand-button">Stand</button>
+                </div>
+                <div id="bj-player-col">
+                    <span id="bj-player-title">Player</span>
+                    <span id="bj-player-hand-elm">Hand: </span>
+                    <div id="bj-player-cards-container"></div>
+                </div>
+                <div id="bj-dealer-col">
+                    <span id="bj-dealer-title">Dealer</span>
+                    <span id="bj-dealer-hand-elm">Hand: </span>
+                    <div id="bj-dealer-cards-container"></div>
+                </div>
+            </section>
+            
+            : <ProjectMenu setActiveL={setActive} />
+            }
         </>
     )
 }
